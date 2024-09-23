15110e4cd4fec8f44e7fb0517ac2972eb9ed67fd
asifalam515 <asibulalam515@gmail.com>
2024-09-23 08:50:05 +0600

    cors middleware added

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const phones = require("./phones.json");
 const app = express();
 const port = 3000;
+var cors = require("cors");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World! from express");
+});
+app.get("/phones", (req, res) => {
+  res.send(phones);
+});
+app.get("/phones/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  //   id ta peye jabo
+
+  const phone = phones.find((phone) => phone.id === id) || {};
+  res.send(phone);
+  console.log(phone);
 });
 
 app.listen(port, () => {
